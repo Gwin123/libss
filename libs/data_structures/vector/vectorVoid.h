18820943b91e79b98f41bd72ee4440f0fefdd911
Gwin_disel <danya.savin.03@mail.ru>
2022-02-04 19:35:45 +0300

    init vectorVoid

--- libs/data_structures/vector/vectorVoid.h
+++ libs/data_structures/vector/vectorVoid.h
@@ -5,4 +5,27 @@
 #ifndef LAST_VECTORVOID_H
 #define LAST_VECTORVOID_H
 
+#include <stdint.h>
+#include <assert.h>
+#include <memory.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct vectorVoid {
+    void *data;
+    size_t size;
+    size_t capacity;
+    size_t baseTypeSize;
+} vectorVoid;
+
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+void shrinkToFitV(vectorVoid *v);
+
+void clearV(vectorVoid *v);
+
+void deleteVectorV(vectorVoid *v);
+
 #endif //LAST_VECTORVOID_H
